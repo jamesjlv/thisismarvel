644d52cac90746b8af6881f47498afdb2e8cc987
James Leal Vieira <james.leal2@gmail.com>
2023-08-26 22:56:41 -0300

    FEAT: added buttons to welcome screen

--- src/presentation/jsx/authentication/welcome/index.tsx
+++ src/presentation/jsx/authentication/welcome/index.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 
-import { Container } from "./styles";
+import {
+  ButtonWrapper,
+  Container,
+  CreateAccountButton,
+  SignInButton,
+  WelcomeTitle,
+  WelcomeWrapper,
+} from "./styles";
+import { StatusBar } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-export const WelcomeScreen = () => {
-  return <Container></Container>;
+export const WelcomeScreen: React.FC = () => {
+  const { navigate } = useNavigation();
+
+  return (
+    <>
+      <StatusBar hidden />
+      <Container source={require("@assets/images/Welcome.jpg")}>
+        <WelcomeWrapper>
+          <WelcomeTitle text="O universo Marvel na palma da sua mão." />
+          <ButtonWrapper>
+            <CreateAccountButton title="Cadastrar-se" />
+            <SignInButton
+              title="Faça login"
+              onPress={() => {
+                navigate("Login");
+              }}
+              types="secondary"
+            />
+          </ButtonWrapper>
+        </WelcomeWrapper>
+      </Container>
+    </>
+  );
 };
--- src/presentation/jsx/authentication/welcome/styles.tsx
+++ src/presentation/jsx/authentication/welcome/styles.tsx
@@ -1,3 +1,37 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
+import { Button, ButtonProps, Title } from "@/presentation/components";
+import { scale } from "@/shared/styles";
 
-export const Container = styled.View``;
+export const Container = styled.ImageBackground.attrs({
+  resizeMode: "cover",
+})`
+  flex: 1;
+  flex-direction: column-reverse;
+`;
+
+export const WelcomeWrapper = styled.View`
+  ${({ theme: { moderateSize } }) => css`
+    padding: ${moderateSize.xLarge} ${moderateSize.large};
+  `}
+`;
+
+export const WelcomeTitle = styled(Title)``;
+
+export const ButtonWrapper = styled.View`
+  margin: ${({ theme }) => theme.moderateSize.xxLarge} 0;
+`;
+
+export const CreateAccountButton = styled(Button).attrs(
+  ({ theme: { sizes } }) =>
+    ({
+      elements: {
+        containerProps: {
+          style: {
+            marginBottom: scale(sizes.large),
+          },
+        },
+      },
+    }) as ButtonProps,
+)``;
+
+export const SignInButton = styled(Button)``;
