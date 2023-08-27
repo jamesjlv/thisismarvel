7c893058a3bd4ea801c31501123f7df6dbc5c747
James Leal Vieira <james.leal2@gmail.com>
2023-08-27 11:07:25 -0300

    FEAT: create form on signin screen

--- src/presentation/jsx/authentication/signin/index.tsx
+++ src/presentation/jsx/authentication/signin/index.tsx
@@ -1,7 +1,59 @@
 import React from "react";
 
-import { Container } from "./styles";
+import {
+  ButtonConfirm,
+  ButtonContainer,
+  Container,
+  Description,
+  FormContent,
+  GoBackButton,
+  Header,
+  SignInTitle,
+  TextInput,
+} from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 export const SignInScreen = () => {
-  return <Container source={require("@assets/images/SignIn.jpg")}></Container>;
+  const { goBack } = useNavigation();
+
+  return (
+    <Container source={require("@assets/images/SignIn.jpg")}>
+      <Header>
+        <SignInTitle text="Cadastro" />
+        <Description text="Informe os dados abaixo para se cadastrar" />
+      </Header>
+      <FormContent>
+        <TextInput
+          placeHolder="Informe seu nome completo"
+          title="Nome Completo"
+          iconName="Person"
+          type="primary"
+        />
+        <TextInput
+          placeHolder="Informe seu e-mail"
+          title="E-mail"
+          iconName="Mail"
+          type="primary"
+        />
+        <TextInput
+          placeHolder="Informe sua senha"
+          iconName="Key"
+          title="Senha"
+          secureTextEntry
+          type="primary"
+        />
+        <TextInput
+          placeHolder="Confirme sua senha"
+          iconName="Key"
+          title="Confirmação de Senha"
+          secureTextEntry
+          type="primary"
+        />
+      </FormContent>
+      <ButtonContainer>
+        <ButtonConfirm title="Cadastrar" />
+        <GoBackButton types="secondary" title="Voltar" onPress={goBack} />
+      </ButtonContainer>
+    </Container>
+  );
 };
--- src/presentation/jsx/authentication/signin/styles.tsx
+++ src/presentation/jsx/authentication/signin/styles.tsx
@@ -1,5 +1,54 @@
+import {
+  Button,
+  ButtonProps,
+  Input,
+  Subtitle,
+  Title,
+} from "@/presentation/components";
+import { scale } from "@/shared/styles";
 import styled from "styled-components/native";
 
 export const Container = styled.ImageBackground`
   flex: 1;
+`;
+
+export const SignInTitle = styled(Title)``;
+
+export const Description = styled(Subtitle)``;
+
+export const Header = styled.View`
+  align-items: center;
+  margin-top: ${scale(130)}px;
+  margin-bottom: ${({ theme }) => theme.moderateSize.xLarge};
+`;
+
+export const TextInput = styled(Input).attrs({
+  type: "primary",
+})`
+  max-height: ${scale(88)}px;
+`;
+
+export const ButtonConfirm = styled(Button).attrs(
+  ({ theme }) =>
+    ({
+      elements: {
+        containerProps: {
+          style: {
+            marginBottom: scale(theme.sizes.large),
+          },
+        },
+      },
+    }) as ButtonProps,
+)``;
+
+export const GoBackButton = styled(Button)``;
+
+export const FormContent = styled.View`
+  flex: 1;
+  margin: ${({ theme }) => theme.moderateSize.large};
+`;
+
+export const ButtonContainer = styled.View`
+  padding: ${({ theme }) => theme.moderateSize.large};
+  margin-bottom: ${({ theme }) => theme.moderateSize.large};
 `;
