ffe36b0213d962965ec915a72440645ae8033cbf
James Leal Vieira <james.leal2@gmail.com>
2023-08-27 10:30:22 -0300

    FEAT: added functions to navigate on welcome screen

--- src/presentation/jsx/authentication/welcome/index.tsx
+++ src/presentation/jsx/authentication/welcome/index.tsx
@@ -10,9 +10,25 @@ import {
 } from "./styles";
 import { StatusBar } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { Routes } from "@/main/routes/enums/Routes";
 
 export const WelcomeScreen: React.FC = () => {
   const { navigate } = useNavigation();
+
+  const handleNavigateLogin = () => {
+    try {
+      navigate(Routes.Login);
+    } catch (error) {
+      console.error("Não foi possível redirecionar para o login.");
+    }
+  };
+  const handleNavigateNewAccount = () => {
+    try {
+      navigate(Routes.SignIn);
+    } catch (error) {
+      console.error("Não foi possível redirecionar para o cadastro.");
+    }
+  };
 
   return (
     <>
@@ -21,12 +37,13 @@ export const WelcomeScreen: React.FC = () => {
         <WelcomeWrapper>
           <WelcomeTitle text="O universo Marvel na palma da sua mão." />
           <ButtonWrapper>
-            <CreateAccountButton title="Cadastrar-se" />
+            <CreateAccountButton
+              title="Cadastrar-se"
+              onPress={handleNavigateNewAccount}
+            />
             <SignInButton
               title="Faça login"
-              onPress={() => {
-                navigate("Login");
-              }}
+              onPress={handleNavigateLogin}
               types="secondary"
             />
           </ButtonWrapper>
