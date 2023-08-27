c299882bdec15233a21cb6fd18e3e5ae90514b05
James Leal Vieira <james.leal2@gmail.com>
2023-08-27 10:30:37 -0300

    FEAT: create signin screen

---
+++ src/presentation/jsx/authentication/signin/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+export const SignInScreen = () => {
+  return <Container source={require("@assets/images/SignIn.jpg")}></Container>;
+};
