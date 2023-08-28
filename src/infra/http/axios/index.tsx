2ad7e9651f523fa882bb904b456e861e38237c02
James Leal Vieira <james.leal2@gmail.com>
2023-08-27 21:34:29 -0300

    FEAT: ajust axios to handle auth on marvel api

--- src/infra/http/axios/index.tsx
+++ src/infra/http/axios/index.tsx
@@ -1,10 +1,26 @@
 /* eslint-disable class-methods-use-this */
 import axios, { AxiosResponse } from "axios";
 import { HttpRequest, HttpResponse, HttpClient } from "@/data/protocols/http";
+import Config from "react-native-config";
+import Crypto from "crypto-js/md5";
 
 export class AxiosHttpClient implements HttpClient {
   async request(data: HttpRequest): Promise<HttpResponse> {
+    const currentDate = new Date();
+    const timestamp = currentDate.toISOString();
+
+    axios.interceptors.request.use((config) => {
+      config.params = config.params || {};
+      config.params["apikey"] = Config.MARVEL_API_PUBLIC;
+      config.params["ts"] = timestamp;
+      config.params["hash"] = Crypto(
+        `${timestamp}${Config.MARVEL_API_PRIVATE}${Config.MARVEL_API_PUBLIC}`,
+      );
+      return config;
+    });
+
     let axiosResponse: AxiosResponse;
+
     try {
       axiosResponse = await axios.request({
         url: data.url,
