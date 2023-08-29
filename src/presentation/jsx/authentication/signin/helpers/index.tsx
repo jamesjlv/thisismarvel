3548ba26e0842a6d8d6d23412e03887ab32b73c9
James Leal Vieira <james.leal2@gmail.com>
2023-08-29 11:14:08 -0300

    CHORE: create yup validation helper for signin screen

---
+++ src/presentation/jsx/authentication/signin/helpers/index.tsx
@@ -0,0 +1,20 @@
+import { EMAIL_VALIDATOR, NAME_VALIDATOR } from "@/shared";
+
+import * as yup from "yup";
+export const YUP_VALIDATION = yup
+  .object({
+    email: yup
+      .string()
+      .required("Obrigatório informar o e-mail.")
+      .matches(EMAIL_VALIDATOR, { message: "Utilize um e-mail válido." }),
+    name: yup
+      .string()
+      .required("Obrigatório informar o nome completo.")
+      .matches(NAME_VALIDATOR, { message: "Informe o nome completo" }),
+    password: yup.string().required("Obrigatório informar uma senha."),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), undefined], "Confirmação de senha incorreta")
+      .required("Obrigatório confirmar a senha"),
+  })
+  .required();
