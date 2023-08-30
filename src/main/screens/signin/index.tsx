bfe97957b6d76a29dffd894f29de9b75bfb22eeb
James Leal Vieira <james.leal2@gmail.com>
2023-08-29 22:48:04 -0300

    CHORE: removing older signin screen from screens root

--- src/main/screens/signin/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { SignInScreen } from "@/presentation";
-import { manufactureRegisterUser } from "@/main/services/user/remote-register-user";
-
-export const ManufactureSignInScreen: React.FC = () => {
-  return (
-    <SignInScreen handleCreateNewUserAccount={manufactureRegisterUser()} />
-  );
-};
