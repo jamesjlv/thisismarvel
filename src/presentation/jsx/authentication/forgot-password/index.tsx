0c4311a609f37d88a8851569fad052fef2a9aa4d
James Leal Vieira <james.leal2@gmail.com>
2023-08-31 00:22:56 -0300

    CHORE: added validation email and hook form to forgot password screen

--- src/presentation/jsx/authentication/forgot-password/index.tsx
+++ src/presentation/jsx/authentication/forgot-password/index.tsx
@@ -10,8 +10,39 @@ import {
   Header,
   NextButton,
 } from "./styles";
+import { ForgotPasswordScreenProps } from "./props";
+import { useRoute } from "@react-navigation/native";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { YUP_VALIDATION } from "./helpers";
 
-export const ForgotPasswordScreen: React.FC = () => {
+export const ForgotPasswordScreen: React.FC<ForgotPasswordScreenProps> = ({
+  handleSendEmailWithCodeForResetingPassword,
+}) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<{ email: string }>({
+    defaultValues: {
+      email: "",
+    },
+    resolver: yupResolver(YUP_VALIDATION),
+  });
+
+  const handleSendEmailOneTimeCodePassword: SubmitHandler<{
+    email: string;
+  }> = async ({ email }) => {
+    try {
+      const data = await handleSendEmailWithCodeForResetingPassword.exec({
+        email,
+      });
+      console.log("Enviou");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Container source={require("@assets/images/ForgotPassword.png")}>
       <Content>
@@ -20,12 +51,32 @@ export const ForgotPasswordScreen: React.FC = () => {
           <ForgotDescription text="Informe seu usuário para prosseguir" />
         </Header>
         <FormContainer>
-          <EmailInput
-            title="Usuário"
-            placeHolder="Informe seu e-mail"
-            type="primary"
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <EmailInput
+                title="Usuário"
+                placeHolder="exemplo@mail.com.br"
+                type="primary"
+                iconName="Person"
+                onChangeText={(e) => onChange(e.toLowerCase())}
+                onBlur={onBlur}
+                value={value}
+                hasError={!!errors?.email?.message}
+                errorMessage={errors?.email?.message}
+                autoCapitalize="none"
+              />
+            )}
+            name="email"
           />
-          <NextButton title="avançar" types="primary" onPress={() => {}} />
+          <NextButton
+            title="avançar"
+            types="primary"
+            onPress={handleSubmit(handleSendEmailOneTimeCodePassword)}
+          />
         </FormContainer>
       </Content>
     </Container>
--- src/presentation/jsx/authentication/forgot-password/props.tsx
+++ src/presentation/jsx/authentication/forgot-password/props.tsx
@@ -1,3 +1,9 @@
+import { RemoteForgotPasswordServiveNamespace } from "@/domain";
+
 export interface ForgotPasswordScreenRouteProps {
   email?: string;
+}
+
+export interface ForgotPasswordScreenProps {
+  handleSendEmailWithCodeForResetingPassword: RemoteForgotPasswordServiveNamespace.Interface;
 }
