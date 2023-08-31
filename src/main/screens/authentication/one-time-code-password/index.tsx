fb13e89f2efc4b330351e9a693bdbe21ba0f05a6
James Leal Vieira <james.leal2@gmail.com>
2023-08-31 00:40:53 -0300

    CHORE: creating manufacture of otp screen

---
+++ src/main/screens/authentication/one-time-code-password/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { OneTimeCodePasswordScreen } from "@/presentation";
+
+export const ManufactureOneTimeCodePasswordScreen: React.FC = () => (
+  <OneTimeCodePasswordScreen />
+);
