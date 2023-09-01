4ceb531412193afd1a7df288008744c104d78357
James Leal Vieira <james.leal2@gmail.com>
2023-08-31 23:37:51 -0300

    FEAT: added header and inputs to recovery type

--- src/presentation/jsx/authentication/password-recovery/index.tsx
+++ src/presentation/jsx/authentication/password-recovery/index.tsx
@@ -1,14 +1,47 @@
 import React from "react";
 
-import { Container } from "./styles";
+import {
+  ButtonContainer,
+  Container,
+  Content,
+  FormContainer,
+  Header,
+  KeyboardAvoid,
+  PasswordInput,
+  PasswordRecoveryDescription,
+  PasswordRecoveryTitle,
+  RecoverButton,
+} from "./styles";
 import { PasswordRecoveryScreenProps } from "./props";
 
 export const PasswordRecoveryScreen: React.FC<
   PasswordRecoveryScreenProps
 > = () => {
   return (
-    <Container
-      source={require("@assets/images/PasswordRecovery.png")}
-    ></Container>
+    <Container source={require("@assets/images/PasswordRecovery.png")}>
+      <KeyboardAvoid>
+        <Content>
+          <Header>
+            <PasswordRecoveryTitle text="Informe a nova senha" />
+            <PasswordRecoveryDescription text="Informe abaixo a nova senha para sua conta." />
+          </Header>
+          <FormContainer>
+            <PasswordInput
+              title="Senha"
+              placeHolder="Informe a sua senha"
+              type="primary"
+            />
+            <PasswordInput
+              title="Confirmação de senha"
+              placeHolder="Confirme sua senha"
+              type="primary"
+            />
+            <ButtonContainer>
+              <RecoverButton title="recuperar" onPress={() => {}} />
+            </ButtonContainer>
+          </FormContainer>
+        </Content>
+      </KeyboardAvoid>
+    </Container>
   );
 };
--- src/presentation/jsx/authentication/password-recovery/styles.tsx
+++ src/presentation/jsx/authentication/password-recovery/styles.tsx
@@ -1,5 +1,72 @@
+import {
+  Button,
+  Input,
+  Subtitle,
+  Title,
+  ButtonProps,
+} from "@/presentation/components";
+import { scale } from "@/shared/styles";
+import { Platform } from "react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.ImageBackground`
   flex: 1;
 `;
+
+export const KeyboardAvoid = styled.KeyboardAvoidingView.attrs({
+  behavior: Platform.OS === "ios" ? "padding" : "height",
+})`
+  flex: 1;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+export const Header = styled.View`
+  margin: 0 ${scale(77)}px;
+  margin-bottom: ${({ theme }) => theme.moderateSize.xxMedium};
+`;
+
+export const PasswordRecoveryTitle = styled(Title)`
+  text-align: center;
+  flex: none;
+`;
+
+export const PasswordRecoveryDescription = styled(Subtitle)`
+  text-align: center;
+`;
+
+export const FormContainer = styled.View`
+  margin: 0 ${scale(58)}px;
+`;
+
+export const PasswordInput = styled(Input).attrs({
+  iconName: "Key",
+  isPassword: true,
+  secureTextEntry: true,
+  elements: {
+    container: {
+      style: {
+        flex: -1,
+      },
+    },
+  },
+})``;
+
+export const ButtonContainer = styled.View`
+  flex: none;
+`;
+
+export const RecoverButton = styled(Button).attrs(
+  ({ theme }) =>
+    ({
+      elements: {
+        containerProps: {
+          style: {
+            marginTop: scale(theme.sizes.medium),
+          },
+        },
+      },
+    }) as ButtonProps,
+)``;
