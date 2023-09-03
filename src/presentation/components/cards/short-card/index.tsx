fd42610b97c77b1f0d6c26e261348101f1702132
James Leal Vieira <james.leal2@gmail.com>
2023-09-03 19:44:27 -0300

    FEAT: handle data sended to details screen on component short info

--- src/presentation/components/cards/short-card/index.tsx
+++ src/presentation/components/cards/short-card/index.tsx
@@ -3,15 +3,84 @@ import { ShortCardInfoProps } from "./props";
 import { Card, CardOpacity, CardTitle, Press } from "./styles";
 import { useNavigation } from "@react-navigation/native";
 import { Routes } from "@/main/routes/enums/Routes";
+import {
+  ICharactersResultsModel,
+  IComicsResultsModel,
+  IEventsResultsModel,
+} from "@/domain";
 
 export const ShortCardInfo: React.FC<ShortCardInfoProps> = ({
   url,
   title = "Card Title",
   type,
+  data,
 }) => {
   const { navigate } = useNavigation();
+
+  const handleInfo = () => {
+    switch (type) {
+      case "comics":
+        const comics = data as IComicsResultsModel;
+        return {
+          title: comics.title,
+          subtitle: "Marvel",
+          interesting: [
+            { title: "Histórias", qtd: comics.stories.available },
+            { title: "Eventos", qtd: comics.events.available },
+            { title: "Heróis", qtd: comics.characters?.available },
+          ],
+          description: comics.description,
+        };
+      case "characters":
+        const characters = data as ICharactersResultsModel;
+        return {
+          title: characters.name,
+          subtitle: "Marvel",
+          interesting: [
+            { title: "Histórias", qtd: characters.stories.available },
+            { title: "Eventos", qtd: characters.events.available },
+            { title: "Séries", qtd: characters.series.available },
+            { title: "Quadrinhos", qtd: characters.comics.available },
+          ],
+          description: characters.description,
+        };
+      case "events":
+        const events = data as IEventsResultsModel;
+        return {
+          title: events.title,
+          subtitle: "Marvel",
+          interesting: [
+            { title: "Histórias", qtd: events.stories.available },
+            { title: "Heróis", qtd: events.characters.available },
+            { title: "Séries", qtd: events.series.available },
+            { title: "Quadrinhos", qtd: events.comics.available },
+          ],
+          description: events.description,
+        };
+
+      case "series":
+        const series = data as IEventsResultsModel;
+        return {
+          title: series.title,
+          subtitle: "Marvel",
+          interesting: [
+            { title: "Histórias", qtd: series.stories.available },
+            { title: "Heróis", qtd: series.characters.available },
+            { title: "Autores", qtd: series.creators.available },
+            { title: "Quadrinhos", qtd: series.comics.available },
+          ],
+          description: series.description,
+        };
+
+      default:
+        break;
+    }
+  };
+
   return (
-    <Press onPress={() => navigate(Routes.Details, { type, title, url })}>
+    <Press
+      onPress={() => navigate(Routes.Details, { type, url, ...handleInfo() })}
+    >
       <Card
         source={{
           uri: url.includes("image_not_available")
--- src/presentation/components/cards/short-card/props.tsx
+++ src/presentation/components/cards/short-card/props.tsx
@@ -1,5 +1,17 @@
+import {
+  ICharactersResultsModel,
+  IComicsResultsModel,
+  IEventsResultsModel,
+  ISeriesResultsModel,
+} from "@/domain";
+
 export type ShortCardInfoProps = {
   url: string;
   title: string;
   type: "events" | "characters" | "series" | "comics";
+  data:
+    | ICharactersResultsModel
+    | ISeriesResultsModel
+    | IComicsResultsModel
+    | IEventsResultsModel;
 };
