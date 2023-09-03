66403320c16248c2f93edcd4ba73bfa294e043e7
James Leal Vieira <james.leal2@gmail.com>
2023-09-03 19:54:08 -0300

    CHORE: exporting usecase

--- src/domain/services/index.tsx
+++ src/domain/services/index.tsx
@@ -4,3 +4,4 @@ export * from "./characters/remote-get-characters";
 export * from "./comics/remote-get-comics";
 export * from "./series/remote-get-series";
 export * from "./events/remote-get-events";
+export * from "./characters/remote-get-characters-comics-by-id";
