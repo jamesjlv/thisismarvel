ebdb4b272690e1d539b4d81cb50a2ac95695e36f
James Leal Vieira <james.leal2@gmail.com>
2023-09-05 12:16:17 -0300

    CHORE: adding loading state to short card

--- src/presentation/components/cards/short-card/index.tsx
+++ src/presentation/components/cards/short-card/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { ShortCardInfoProps } from "./props";
 import { Card, CardOpacity, CardTitle, Press } from "./styles";
 import { useNavigation } from "@react-navigation/native";
 import { Routes } from "@/main/routes/enums/Routes";
-
 import { handleInfo } from "@/shared";
+import { ActivityIndicator } from "react-native";
 
 export const ShortCardInfo: React.FC<ShortCardInfoProps> = ({
   url,
@@ -13,6 +13,7 @@ export const ShortCardInfo: React.FC<ShortCardInfoProps> = ({
   data,
 }) => {
   const { navigate } = useNavigation();
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <Press
@@ -26,10 +27,18 @@ export const ShortCardInfo: React.FC<ShortCardInfoProps> = ({
             ? "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/portrait_incredible.jpg"
             : url,
         }}
+        onLoadEnd={() => setIsLoading(false)}
       >
-        <CardOpacity colors={["rgba(0, 0, 0, 0.00)", "rgba(0, 0, 0, 0.41)"]}>
-          <CardTitle>{title}</CardTitle>
-        </CardOpacity>
+        {isLoading ? (
+          <ActivityIndicator
+            size="small"
+            style={{ flex: 1, backgroundColor: "#ccc" }}
+          />
+        ) : (
+          <CardOpacity colors={["rgba(0, 0, 0, 0.00)", "rgba(0, 0, 0, 0.41)"]}>
+            <CardTitle>{title}</CardTitle>
+          </CardOpacity>
+        )}
       </Card>
     </Press>
   );
