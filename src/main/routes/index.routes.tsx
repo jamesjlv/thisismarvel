54b4eeb7d63a3e1909e8f5b2d6df5934662f848f
James Leal Vieira <james.leal2@gmail.com>
2023-09-08 17:59:14 -0300

    CHORE: handle user already logged

--- src/main/routes/index.routes.tsx
+++ src/main/routes/index.routes.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { InitialAuthentizationRoutes } from "./authentication/index.routes";
-import { NavigationContainer } from "@react-navigation/native";
 import {
-  TransitionPresets,
-  createStackNavigator,
-} from "@react-navigation/stack";
+  NavigationContainer,
+  NavigationContainerRef,
+} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { Routes as RoutesEnum, Stacks } from "./enums/Routes";
 import { InitialAuthorizedRoutes } from "./authorized/index.routes";
+import { useAuthHook } from "@/presentation/hooks/providers/auth";
+
+export const navigationRef = React.createRef<NavigationContainerRef<any>>();
 
 export const Routes: React.FC = () => {
   const Stack = createStackNavigator();
-
+  const { token } = useAuthHook();
+  useEffect(() => {
+    if (token) {
+      navigationRef.current?.navigate(Stacks.Authorized, {
+        screen: RoutesEnum.Home,
+      });
+    }
+  }, [token]);
   return (
-    <NavigationContainer independent={true}>
+    <NavigationContainer ref={navigationRef} independent={true}>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen
           name={Stacks.Authorization}
